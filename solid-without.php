ecc90b3ea369bca24068ded312dd71f6491971b6
José Lisboa Filho <104661333+Proceed15@users.noreply.github.com>
2024-11-23 09:32:40 -0300

    Update Aula SOLID Parte 1

---
+++ solid-without.php
@@ -0,0 +1,30 @@
+<?php
+
+class NotificationService {
+
+    public function sendEmail($email, $message) {
+        echo "Enviado e-mail para $email: $message";
+    }
+    
+    public function sendSMS($phoneNumber, $message) {
+        echo "Enviado SMS para $phoneNumber: $message";
+    }
+
+    public function notify($user, $message) {
+        if (!empty($user['email'])) {
+            $this->sendEmail($user['email'], $message);
+        }
+        
+        if (!empty($user['phone'])) {
+            $this->sendSMS($user['phone'], $message);
+        }
+    }
+    
+}
+
+$notificationService = new NotificationService();
+$user = [
+    'email' => 'user@dominio.com',
+    'phone' => '1511223344'
+];
+$notificationService->notify($user, "<br />Bem vindo ao sistema!<hr />");
